1c92832ccfc408c3b1d44fbf78d5dd9b49cedfdf
Hui Sheng @ Desktop <deathezer10@live.com.sg>
2022-10-16 00:34:29 +0800

    Singpass QR works

--- lib/singpassloginpage.dart
+++ lib/singpassloginpage.dart
@@ -6,50 +6,19 @@ class SingPassLoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Center(
-            child: Text(
-          'Sign In',
-          style: TextStyle(fontFamily: 'DMSans'),
-        )),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.add_alert),
-            tooltip: 'Show Snackbar',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('This is a snackbar')));
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.navigate_next),
-            tooltip: 'Go to the next page',
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute<void>(
-                builder: (BuildContext context) {
-                  return Scaffold(
-                    appBar: AppBar(
-                      title: const Text('Next page'),
-                    ),
-                    body: const Center(
-                      child: Text(
-                        'This is the next page',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                    ),
-                  );
-                },
-              ));
-            },
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text(
-          'This is the home page',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
+          centerTitle: true,
+          title: const Text(
+            'Sign In',
+            style: TextStyle(fontFamily: 'DMSans'),
+          )),
+      body: InkWell(
+          onTap: () => {},
+          splashColor: Colors.white10,
+          child: Ink.image(
+              alignment: Alignment.topCenter,
+              image: const AssetImage("images/singpass_qr.png"))),
     );
   }
 }
